dcd533d64e2e80f565bc921e13ff257852083f6f
madhu-famelyn <madhu@famelyn.com>
2025-10-30 18:16:21 +0530

    updated form

--- lexfactos/src/Components/Comments/AddComments.js
+++ lexfactos/src/Components/Comments/AddComments.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/UserContext";
 import {
   FaStar,
-  FaRegCommentDots,
   FaChevronDown,
   FaChevronUp,
   FaPlus,
